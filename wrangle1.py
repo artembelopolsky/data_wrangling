4f508d8e6ea3709f8766afefbe4e8b2c120fd8cc
artembelopolsky <artem.belopolsky@gmail.com>
2018-08-27 22:35:55 +0200

    added function to stack the variables
    Added functions, cleaned the code, added comparison of two methods to create a new column based on values of several other columns

--- wrangle1.py
+++ wrangle1.py
@@ -1,24 +1,42 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Aug 23 14:48:41 2018
-
 @author: Artem Belopolsky
 """
 
-# Import modules
+#==============================================================================
+# Load modules
+#==============================================================================
+
 import pandas as pd
 import numpy as np
+import time
 
-# Create a dataframe
-raw_data = {'first_name': ['Jason', 'Molly', 'Helen', 'Laura', 'Katja'],
+
+#==============================================================================
+# Create a dataframe 
+#==============================================================================
+
+raw_data = {'first_name': ['Jason', 'Andrew', 'Helen', 'Laura', 'Katja'],
         'nationality': ['USA', 'USA', 'France', 'Netherlands', 'Russia'],
-        'age': [42, 52, 36, 24, 70]}
-df = pd.DataFrame(raw_data, columns = ['first_name', 'nationality', 'age'])
+        'age': [42, 52, 36, 24, 70],
+        'height': [180, 175, 200, 195, 199],
+        'image1': 0, 'image2': 1, 'image3': 1, 'image4': 1}
+
+df = pd.DataFrame(raw_data)
+df_bckup = df.copy() # copy dataframe for speed comparison below
 
 print df
+print '\n\n\n'
 
 
+#==============================================================================
+# Create a new column in a dataframe based on values in other columns 
+# METHOD 1
+# Step1: Create a function to apply to each row of entries
+# Step2: Apply this function to the entries using df.apply()
+#==============================================================================
 
+# Step1
 def label(first, second):
 
     if first == 'Jason' and second =='USA':
@@ -29,9 +47,67 @@ def label(first, second):
 
     return num
 
+# Step 2
+
+tick = time.time()   
 df['label'] = df.apply(lambda row: label(row['first_name'], row['nationality']), axis=1)
+tock = time.time()
+print 'time for df.apply method: ' + str(tock-tick)
+print df
+print '\n\n\n'
+time_apply = tock-tick
+
+#==============================================================================
+# Create a new column in a dataframe based on values in other columns 
+# METHOD 2
+# Using list comprehension
+#==============================================================================
+tic = time.time()
+
+s = [1 if (x == 'Jason' and y == 'USA') else 2 if (x == 'Helen' and y == 'France') else 0 for x,y in zip(df_bckup.first_name, df_bckup.nationality)]
+  
+df_bckup['label'] = s
+toc = time.time()
+time_listcomp = tock-tick
+
+print 'time for list comprehension method: ' + str(tock-tick)
+print 'df.apply() is ' + str(time_listcomp/time_apply) + 'times faster than list comprehention'
+#==============================================================================
+# Create a long table by stacking selected columns  
+#==============================================================================
+def stack_columns(df, colnames_to_stack, new_colname1, new_colname2):
+      """
+      
+      Parameters
+      ----------
+      
+      df : pandas dataframe
+      cols_to_stack : list of column names that need to be stacked together
+      new_colname1 : string containing the name for all stacked columns
+      new_colname2 : string containng the name for value in stacked columns
+      
+      Returns
+      -------
+      A dataframe with two new columns instead of the columns to be stacked
+      """
+      # Make a list of column names to keep 
+      keep_colnames = list(df.columns.drop(colnames_to_stack))
+      # Stack selected columns
+      df = df.set_index(keep_colnames).stack().reset_index()
+      
+      # Figure out the level of the new column 
+      name_col = 'level_' + str(len(keep_colnames))
+      # Replace that name with own name
+      df = df.rename(columns={0:new_colname2, name_col:new_colname1})
+      
+      
+      return df
+
+
+# List of column names to keep
+drop_cols = ['image1', 'image2', 'image3', 'image4']
+# Stack the columns
+df = stack_columns(df, drop_cols, 'image', 'correct')
 
 print df
-
-
-
+print '\n\n\n'
